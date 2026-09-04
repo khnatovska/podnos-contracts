3c0b6070b0e5b729a434f65e14af1baa50a3a8cb
Ksenia Hnatovska <k.hnatovska@gmail.com>
2026-09-04 13:42:06 +0300

    added CI

--- eslint.config.js
+++ eslint.config.js
@@ -3,28 +3,28 @@ import tseslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
 
 export default tseslint.config(
-  {
-    ignores: ['dist/**', 'coverage/**', 'node_modules/**'],
-  },
-  js.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+    {
+        ignores: ['dist/**', 'coverage/**', 'node_modules/**'],
     },
-    rules: {
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
-      ],
+    js.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+            ],
+        },
     },
-  },
-  {
-    files: ['**/*.js'],
-    extends: [tseslint.configs.disableTypeChecked],
-  },
-  prettier,
+    {
+        files: ['**/*.js'],
+        extends: [tseslint.configs.disableTypeChecked],
+    },
+    prettier,
 );
